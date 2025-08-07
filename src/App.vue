bb935a71a13c212c23e504a0378f172a2b833c57
York <a@a.com>
2025-08-08 00:36:54 +0800

    feat: 新增 chartjs 套件，並使用 multi datalabel 來顯示 stack bar 的加總

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,128 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { Bar } from "vue-chartjs";
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  type ChartOptions,
+  type ChartData,
+} from "chart.js";
+import ChartDataLabels from "chartjs-plugin-datalabels";
+
+const chartData: ChartData<"bar"> = {
+  labels: ["8月5日", "8月6日", "8月7日", "8月8日"],
+  datasets: [
+    {
+      label: "mike",
+      data: [5, 0, 20, 0],
+      backgroundColor: "#42A5F5",
+    },
+    {
+      label: "anne",
+      data: [3, 10, 0, 4],
+      backgroundColor: "#66BB6A",
+    },
+    {
+      label: "bill",
+      data: [0, 15, 3, 0],
+      backgroundColor: "#FFA726",
+    },
+  ],
+};
+
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  ChartDataLabels
+);
+
+const chartOptions: ChartOptions<"bar"> = {
+  responsive: true,
+  scales: {
+    x: { stacked: true },
+    y: { stacked: true, grace: "5%" },
+  },
+  onClick: (e, el, chart) => {
+    // console.log("event :>> ", e);
+    console.log("el :>> ", el);
+    console.log("chart :>> ", chart);
+
+    if (el.length > 0) {
+      const datasetIndex = el[0].datasetIndex;
+      const dataIndex = el[0].index;
+      const value = chart.data.datasets[datasetIndex].data[dataIndex];
+      console.log(
+        `Clicked on dataset ${datasetIndex}, data index ${dataIndex}, value: ${value}`
+      );
+      console.log("label :>> ", chart.data.labels?.[dataIndex]);
+      console.log("value :>> ", value);
+    }
+  },
+  plugins: {
+    legend: {
+      position: "right",
+    },
+    tooltip: {
+      enabled: false,
+    },
+    datalabels: {
+      labels: {
+        value: {
+          color: "#000",
+          font: {
+            weight: "bold",
+            size: 16,
+          },
+          formatter: (value: number) => {
+            return !!value ? `${value}` : "";
+          },
+        },
+        sum: {
+          anchor: "end",
+          align: "top",
+          color: "red",
+          font: {
+            weight: "bold",
+            size: 20,
+          },
+          clip: true,
+          formatter: (v: number, ctx) => {
+            const datasets = ctx.chart.data.datasets;
+            const dataIndex = ctx.dataIndex;
+            if (ctx.datasetIndex === datasets.length - 1) {
+              const sum = datasets
+                .map((ds) => ds.data[dataIndex] as number)
+                .reduce((a, b) => a + b, 0);
+              return sum;
+            }
+            return "";
+          },
+        },
+      },
+    },
+  },
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="chart-container">
+    <Bar id="my-chart-id" :options="chartOptions" :data="chartData" />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.chart-container {
+  margin-top: 20px;
+  position: relative;
+  width: 100%;
+  height: 400px;
 }
 </style>
